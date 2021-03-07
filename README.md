9146336edab933356b6d6aba00cffd076ac6ba47
Hugo-Gomez <hugo.gomez9795@gmail.com>
2021-03-07 17:37:02 +0100

    prepare release

---
+++ README.md
@@ -0,0 +1,41 @@
+# Check Mates LoL
+**Release coming soon**
+
+A GUI application to check stats of teammates when arriving on a lobby.
+
+## Installation
+
+Install dependencies (virtual env is recommended)
+
+```bash
+pip install -r requirements.txt
+```
+
+Create storage file from example
+```bash
+cp storage.example.json storage.json
+```
+
+## Usage
+
+You can launch the app with python
+```bash
+python main.py
+```
+
+Or you can create an executable file
+```bash
+pyinstaller "Check Mates Lol.spec"
+```
+
+Then, you can take the folder `Check Mates LoL` inside `dist` and put it wherever 
+you like.<br>
+Inside this folder, you'll find an executable file called `Check Mates LoL`, which is
+the program. You can now create a shortcut from this file and place it in your Desktop 
+to use it like a normal program.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- main.py
+++ main.py
@@ -1,57 +1,75 @@
 import PySimpleGUI as sg
 import webbrowser
+import json
 
-# TODO : save infos
+# TODO : tests and error handling
 # TODO : add more languages
 # TODO : add more search engines
-# TODO : clean, fragment in multiple files and comment
+# TODO : clean-up and comment
 
 # TEST VARIABLES
 FR_TEST_message = 'JakubMichalak a rejoint le salon\nLubiar a rejoint le salon\nÉ T a rejoint le salon\nTorethiel a ' \
                'rejoint le salon\nCasual Dada a rejoint le salon'
 
 # CONSTANTS
-regions = ['EUNE', 'JP', 'LAN', 'NA', 'RU', 'BR', 'EUW', 'KR', 'LAS', 'OCE', 'TR']
-languages = ['FR', 'EN']
-search_engines = {
+REGIONS = ['EUNE', 'JP', 'LAN', 'NA', 'RU', 'BR', 'EUW', 'KR', 'LAS', 'OCE', 'TR']
+LANGUAGES = ['FR', 'EN']
+SEARCH_ENGINES = {
     'LeagueOfGraphs': 'https://www.leagueofgraphs.com/summoner/CML-REGION/CML-USERNAME',
     'op.gg': 'https://CML-REGION.op.gg/summoner/userName=CML-USERNAME'
 }
-lobby_language_message = {
+LOBBY_LANGUAGE_MESSAGE = {
     'FR': ' a rejoint le salon',
     'EN': ' joined the lobby'
 }
 
 
-# PYSIMPLEGUI LAYOUT
-layout = [[sg.Text('Username'), sg.InputText(size=(20, 1), key='username', default_text='Lubiar')],
-          [sg.Text('Region'), sg.InputOptionMenu(regions, size=(7, 1), key='region', default_value='EUW'),
-           sg.Text('Client language'), sg.InputOptionMenu(languages, size=(5, 1), key='language', default_value='FR'),
-           sg.Text('Website'), sg.InputOptionMenu([*search_engines.keys()], size=(20, 1), key='engine', default_value='LeagueOfGraphs')],
-          [sg.Checkbox('Save infos', size=(10, 1), key='save')],
-          [sg.Text('Lobby arrival message'), sg.Multiline(size=(50, 5), key='message', default_text=FR_TEST_message)],
-          [sg.OK('Check Mates', key='search')]]
+def generate_layout(store_dict):
+    # PYSIMPLEGUI LAYOUT
+    layout = [[sg.Text('Username'), sg.InputText(size=(20, 1), key='username', default_text=store_dict['username'])],
+              [sg.Text('Region'), sg.InputOptionMenu(REGIONS, size=(7, 1), key='region', default_value=store_dict['region']),
+               sg.Text('Client language'),
+               sg.InputOptionMenu(LANGUAGES, size=(5, 1), key='language', default_value=store_dict['language']),
+               sg.Text('Website'),
+               sg.InputOptionMenu([*SEARCH_ENGINES.keys()], size=(20, 1), key='engine', default_value=store_dict['engine'])],
+              [sg.Text('Lobby arrival message'),
+               sg.Multiline(size=(50, 5), key='message', default_text=FR_TEST_message),
+               sg.OK('Clear', key='clear')],
+              [sg.OK('Check Mates', key='search'), sg.OK('Clear All', button_color=('white', 'red'), key='clear_all')]]
+    return layout
 
 
 def get_mates(message, username, language):
-    players = message.replace('\n', '').replace(lobby_language_message[language], '|').split('|')
+    players = message.replace('\n', '').replace(LOBBY_LANGUAGE_MESSAGE[language], '|').split('|')
     return [x for x in players if (len(x) > 0 and not x == username)]
 
 
 def open_search_tab(username, region, engine):
-    url = search_engines[engine].replace('CML-USERNAME', username).replace('CML-REGION', region.lower())
+    url = SEARCH_ENGINES[engine].replace('CML-USERNAME', username).replace('CML-REGION', region.lower())
     webbrowser.open_new_tab(url)
 
 
 if __name__ == '__main__':
-    window = sg.Window('Check Mates LoL', layout)
+    with open('storage.json', 'r') as storage_file:
+        storage = json.loads(storage_file.read())
+    window = sg.Window('Check Mates LoL', generate_layout(storage))
     while True:
         event, values = window.read()
+        # Search mates
         if event == 'search':
-            print(values)
             mates = get_mates(values['message'], values['username'], values['language'])
             for mate in mates:
                 open_search_tab(mate, values['region'], values['engine'])
-        if event in (sg.WIN_CLOSED, 'Cancel'):
+        # Clear every field
+        if event == 'clear_all':
+            for key in values.keys():
+                window[key]('')
+        # Clear lobby message
+        if event == 'clear':
+            window['message']('')
+        if event == sg.WIN_CLOSED:
             break
+        # Save infos
+        with open('storage.json', 'w') as storage_file:
+            json.dump(values, storage_file)
     window.close()
